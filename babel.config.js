f051c728a9e0e763d5f0be687126f982f9f9323f
Özer Özay <ozerozay@gmail.com>
2021-08-08 23:44:02 +0300

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  module: {
-    rules: [
-      {
-        test: /.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-        ]
-      }
-    ]
-  }
+  ]
 }
